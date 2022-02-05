ddb57a51a7ebc668c3184ce8f7a01a1e34577c11
胡争辉 <hu@daonao.com>
2022-02-05 08:32:30 +0800

    add asset

--- src/assets/AppAsset.php
+++ src/assets/AppAsset.php
@@ -6,4 +6,9 @@ use app\assets\AppAsset as AppAppAsset;
 
 class AppAsset extends AppAppAsset
 {
+    public $sourcePath = '@vendor/yiisoft/yii2-app-basic/web/css';
+    public $baseUrl = null;
+    public $css = [
+        'site.css',
+    ];
 }
